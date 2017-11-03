56bd0e9f8287b36e266fb08570fba8d5e520642f
TreesLab <trees@gate.sinica.edu.tw>
2017-11-03 13:50:36 +0800

    Update example_runs.bat

--- example_runs.bat
+++ example_runs.bat
@@ -10,5 +10,5 @@ rsem-prepare-reference --gtf /home/sctech/Desktop/test_NCLcomparator/Homo_sapien
 ### run NCLcomparator  ###
 ##########################
 
-./NCLcomparator.sh -gtf '/media/cswu/My Passport/toolsDev/NCLcomparator/Homo_sapiens.GRCh38.87.gtf' -thread 6 -index STAR_RSEM_index -read1 '/media/cswu/My Passport/toolsDev/HeLa_reads/SRR1637089_1.fastq.gz' -read2 '/media/cswu/My Passport/toolsDev/HeLa_reads/SRR1637089_1.fastq.gz' -intra '/media/cswu/My Passport/toolsDev/test_NCLcomparator/example/5col/intra' -inter '/media/cswu/My Passport/toolsDev/test_NCLcomparator/example/5col/inter' -sce '/media/cswu/My Passport/toolsDev/test_NCLcomparator/example/SCE/SCE_hg38.bed' -o HeLa  
+./NCLcomparator.sh -gtf '/media/cswu/My Passport/toolsDev/NCLcomparator/Homo_sapiens.GRCh38.87.gtf' -thread 6 -index STAR_RSEM_index -read1 '/media/cswu/My Passport/toolsDev/HeLa_reads/SRR1637089_1.fastq.gz' -read2 '/media/cswu/My Passport/toolsDev/HeLa_reads/SRR1637089_2.fastq.gz' -intra '/media/cswu/My Passport/toolsDev/test_NCLcomparator/example/5col/intra' -inter '/media/cswu/My Passport/toolsDev/test_NCLcomparator/example/5col/inter' -sce '/media/cswu/My Passport/toolsDev/test_NCLcomparator/example/SCE/SCE_hg38.bed' -o HeLa  
 
